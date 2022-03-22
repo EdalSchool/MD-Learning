3438e466fdfdd21a17b384aa90b7e41be53276af
Manuel David Monteagudo <manueldavid.monteagudomosquea@gmail.com>
2022-03-22 16:08:15 -0400

    Creé la clase Warehouse

--- MyFirstBlazorApp/Entities/Warehouse.cs
+++ MyFirstBlazorApp/Entities/Warehouse.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities
 {
-    internal class Bodega
+    public class Warehouse
     {
+        [Key]
+        [StringLength(70)]
+        public string WarehouseId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string WarehouseName { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string WarehouseAddress { get; set; }
+
+        //Relationship with Storage
+        public ICollection<Storage> Storages { get; set; }
     }
 }
