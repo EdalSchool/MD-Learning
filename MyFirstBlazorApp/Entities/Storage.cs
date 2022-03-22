4b78257fba9aa4ec2e9f1c5f801a66b06258ecc9
Manuel David Monteagudo <manueldavid.monteagudomosquea@gmail.com>
2022-03-22 16:08:03 -0400

    Creé la clase Storage

--- MyFirstBlazorApp/Entities/Storage.cs
+++ MyFirstBlazorApp/Entities/Storage.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Entities
 {
-    internal class Storage
+    public class Storage
     {
+        [Key]
+        [StringLength(50)]
+        public string StorageId { get; set; }
+        [Required]
+        public DateTime LastUpdate { get; set; }
+        [Required]
+        public int ProductQuantity { get; set; }
+
+        //Relationship with Product
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
+
+        //Relationship with Warehouse
+        public string WarehouseId { get; set; }
+        public Warehouse Warehouse { get; set; }
+
+        //Relationship with InOuts
+        public ICollection<InOuts> InOuts { get; set; }
     }
 }
