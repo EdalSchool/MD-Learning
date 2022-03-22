6e6917f57d74bbe3f9ec4c91a4d81cd284c65642
Manuel David Monteagudo <manueldavid.monteagudomosquea@gmail.com>
2022-03-22 19:41:30 -0400

    Creé la clase PlazorAppContext

---
+++ MyFirstBlazorApp/DataAccess/BlazorAppContext.cs
@@ -0,0 +1,27 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class BlazorAppContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<InOuts> InOuts { get; set; }
+        public DbSet<Warehouse> Warehouses { get; set; }
+        public DbSet<Storage> Storages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer(@"Server=(localdb)\mssqllocaldb; Database=MyFirstBlazorAppDb; Trusted_Connection=True; MultipleActiveResultSets=true");
+            }
+        }
+    }
+}
