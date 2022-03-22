6ac6c79b187f48428668e18c91b9edacdadfb027
Manuel David Monteagudo <manueldavid.monteagudomosquea@gmail.com>
2022-03-22 16:07:39 -0400

    Creé la clase InOuts

--- MyFirstBlazorApp/Entities/InOuts.cs
+++ MyFirstBlazorApp/Entities/InOuts.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities
 {
-    internal class InOuts
+    public class InOuts
     {
+        [Key]
+        [StringLength(50)]
+        public string InOutId { get; set; }
+        [Required]
+        public DateTime InOutDate { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public bool IsInput { get; set; }
+
+        //Relationship with InOuts
+        public string StorageId { get; set; }
+        public Storage Storage { get; set; }
     }
 }
