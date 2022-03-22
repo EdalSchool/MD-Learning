3f1c5d55d35ed80d14ba56d0466ab76d31eddc1f
Manuel David Monteagudo <manueldavid.monteagudomosquea@gmail.com>
2022-03-22 16:07:48 -0400

    Creé la clase Product

--- MyFirstBlazorApp/Entities/Product.cs
+++ MyFirstBlazorApp/Entities/Product.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities
 {
-    internal class ProductEntity
+    public class Product
     {
+        [Key]
+        [StringLength(10)]
+        public string ProductId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string ProductName { get; set; }
+        [StringLength(600)]
+        public string ProductDescription { get; set; }
+        public int TotalQuantity { get; set; }
+
+        //Relationship with Category
+        public string CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        //Relationship with Storage
+        public ICollection<Storage> Storages { get; set; }
     }
 }
